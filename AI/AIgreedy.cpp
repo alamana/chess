4bd846437873fbad23e51b82441d717fd740d3ad
alamana <alex.lamana92@gmail.com>
2013-04-20 21:33:16 -0400

    fixed greedyai because it was bothering me

--- AI/AIgreedy.cpp
+++ AI/AIgreedy.cpp
@@ -1,5 +1,8 @@
 #include "AIgreedy.h"
 #include "chessmove.h"
+#include <iostream>
+using namespace std;
+
 
 AIgreedy::AIgreedy() {
     name = "Greedy sonofabitch";
@@ -12,26 +15,32 @@ double AIgreedy::computeValue(const int & piece){
 }
 
 int AIgreedy::getNextMove(const int *board, const int & color){
-    int upper = 15;
-    int lower = 9;
-    int returnval = -1;
-    int deltaVal = -10000;
-    if (color == 1){ upper = 7; lower = 1;}
-    for (int j = 0; j < 125; j++){
-	if (board[j] >= lower && board[j] <= upper){
-	    int val = computeValue(board[j]);
-	    set<int> posmoves = getPossibleMoves(board, j, true);
-	    for (set<int>::iterator i = posmoves.begin(); i != posmoves.end(); i++){
-		int elem = *i;
-		int elemVal = computeValue(board[elem]);
-		if ( (elemVal - val) >= deltaVal ){
-		    deltaVal = elemVal - val;
-		    returnval = 1000*elem + j;
-		}
+  cout << color << endl;
+  int upper = 15;
+  int lower = 9;
+  int returnval = -1;
+  int deltaVal = -10000;
+  if (color == 1){ upper = 7; lower = 1;}
+  cout << upper << " " << lower << endl;
+  for (int j = 0; j < 125; j++){
+    if (board[j] >= lower && board[j] <= upper){
+      int val = computeValue(board[j]);
+      set<int> posmoves = getPossibleMoves(board, j, true);
+      for (set<int>::iterator i = posmoves.begin(); i != posmoves.end(); i++){
+	int elem = *i;
+	cout << "j=" << j << " elem=" << elem << endl;
+	if((elem < lower || elem > upper) && elem != -1){
+	    int elemVal = computeValue(board[elem]);
+	    if ( (elemVal - val) >= deltaVal ){
+	      deltaVal = elemVal - val;
+	      returnval = 1000*elem + j;
 	    }
-	}
+	  }
+      }
     }
-    return returnval;
+  }
+  cout << "returnval=" << returnval << endl;
+  return returnval;
 }
 
 int AIgreedy::pawnPromotion(const int*, const int &) { return 4; }
